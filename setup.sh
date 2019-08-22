b7d9151eb0b036c8fd476047b5d39ae890d75a28
Jacob Taxén <jacob.taxen@live.com>
2019-08-22 13:17:33 +0200

    Add script for updating rc-files

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+# Move files to where they should be
+
+ln -f ackrc $HOME/.ackrc
+ln -f screenrc $HOME/.screenrc
+ln -f tmux.conf $HOME/.tmux.conf
+ln -f vimrc $HOME/.vimrc
+ln -f xmonad.hs $HOME/.xmonad/xmonad.hs
+ln -f xmobarrc.hs $HOME/.xmobarrc
+ln -f xprofile $HOME/.xprofile
+ln -f zshenv $HOME/.zshenv
+ln -f zshrc $HOME/.zshrc
+

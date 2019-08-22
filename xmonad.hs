228fcaa0d13564c5f186bf8d9ad66e6a17c256b5
Jacob Taxén <jacob.taxen@live.com>
2019-08-22 10:28:29 +0200

    Add keybinding for locking screen

--- xmonad.hs
+++ xmonad.hs
@@ -1,6 +1,9 @@
 import XMonad
 import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+
+myModMask = mod1Mask
 
 main = do
     xmproc <- spawnPipe "xmobar"
-    xmonad defaultConfig { terminal = "tilda" }
+    xmonad $ defaultConfig { terminal = "tilda" } `additionalKeys` [((myModMask, xK_u), spawn "slock")]

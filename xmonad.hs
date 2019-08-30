55463c71dc572e55f5266ba3f8b970f47ce36352
Jacob Taxén <jacob.taxen@live.com>
2019-08-30 16:23:08 +0200

    Add xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -4,6 +4,19 @@ import XMonad.Util.EZConfig(additionalKeys)
 
 myModMask = mod1Mask
 
+-- Spotify commands
+spotifyRootCommand = "dbus-send --dest=org.mpris.MediaPlayer2.spotify "
+spotifyPlayPauseToggle = spotifyRootCommand ++ "/org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause"
+spotifyNext = spotifyRootCommand ++ "/org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next"
+
 main = do
     xmproc <- spawnPipe "xmobar"
-    xmonad $ defaultConfig { terminal = "tilda" } `additionalKeys` [((myModMask, xK_u), spawn "slock")]
+    xmonad $ defaultConfig { terminal = "tilda"
+    } `additionalKeys` myKeys
+  where
+    myKeys = [ ((myModMask, xK_u), spawn "slock")
+             , ((0, 0x1008ff13), spawn "amixer -q -D pulse sset Master 4%+")
+             , ((0, 0x1008ff11), spawn "amixer -q -D pulse sset Master 4%-")
+             , ((0, 0x1008ff14), spawn spotifyPlayPauseToggle)
+             , ((0, 0x1008ff17), spawn spotifyNext)
+             ]

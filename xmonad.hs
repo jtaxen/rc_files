6cf26bf2a229a5d2b77962b302893cfc5d61f457
Jacob Taxén <jacob.taxen@volvocars.com>
2025-05-13 10:45:48 +0200

    set alacritty as default terminal emulator

--- xmonad.hs
+++ xmonad.hs
@@ -1,9 +1,18 @@
 import XMonad
 import XMonad.Hooks.SetWMName
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.StackSet
 import XMonad.Util.EZConfig(additionalKeys)
 
 myModMask = mod1Mask
+
+
+-- Set size and position of dialog windows
+myManageHook :: ManageHook
+myManageHook = composeAll
+ [isDialog --> doRectFloat (RationalRect (1 / 4) (1 / 4) (1 / 2) (1 / 2))
+ ]
 
 -- Spotify commands
 spotifyRootCommand = "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify "
@@ -11,9 +20,9 @@ spotifyPlayPauseToggle = spotifyRootCommand ++ "/org/mpris/MediaPlayer2 org.mpri
 spotifyNext = spotifyRootCommand ++ "/org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next"
 
 main = do
-    xmonad $ defaultConfig { terminal = "urxvt"
+    xmonad $ defaultConfig { terminal = "alacritty"
                            , startupHook = setWMName "LG3D"
-                           , manageHook = manageDocks <+> manageHook defaultConfig
+                           , manageHook = manageDocks <+> myManageHook
                            , layoutHook = avoidStruts $ layoutHook defaultConfig
                            , borderWidth = 2
     } `additionalKeys` myKeys
